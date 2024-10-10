39bdcc4ba9ce63992c3e1ef5bdf920a6c96039c1
marckent03 <112865938+marckent03@users.noreply.github.com>
2024-10-11 00:43:49 +0800

    Back to zero

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div >
-            <h1>AmberCash PH not FOUND.</h1>
-            <p>Nothing to be found in this url.</p>
-        </div>
-    );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,11 @@
-import Approach from '@/components/Approach'
-import Blogs from '@/components/Blogs'
-import Footer from '@/components/Footer'
-import Grid from '@/components/Grid'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Services from '@/components/Services'
-import { FloatingNav } from '@/components/ui/FloatingNav'
-import { navItems } from '@/data'
-import Head from 'next/head'
 import React from 'react'
-import { FaHome } from 'react-icons/fa'
 
-const HomePage = () => {
+const page = () => {
   return (
-    <main className="relative bg-white flex
-    justify-center items-center felx-col overflow-hidden
-    mx-auto sm:px-10">
-        <div className="text-white max-w-7xl w-full">
-            <Head>
-              <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <FloatingNav
-            navItems={navItems}
-            />
-            <Hero/>
-            <Grid/>
-            <Approach />
-            <Services />
-            <Blogs />
-            <Footer />
-        </div>
-    </main>
+    <div>
+        Hello
+    </div>
   )
 }
 
-export default HomePage
+export default page
